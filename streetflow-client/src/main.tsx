9d1b9b2556742f1c8485fa8c0802d1ffc4a6af50
Muzamil Mir <muzamilmeer598@gmail.com>
2025-07-04 20:08:09 +0530

    Final GitHub Pages setup

---
+++ streetflow-client/src/main.tsx
@@ -0,0 +1,7 @@
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const App = () => <h1 className="text-accent text-3xl font-bold">Welcome to STREETFLOW</h1>;
+
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
